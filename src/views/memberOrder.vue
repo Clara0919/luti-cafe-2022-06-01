3e62d9339009679aee99ff19b0487bd2ef4c5605
Kelly Chang <u2kelly619@gmail.com>
2022-06-20 09:52:23 +0800

    1. if no cart or order, show no item message

--- src/views/memberOrder.vue
+++ src/views/memberOrder.vue
@@ -1,52 +1,48 @@
 <template>
-  <!-- <div class="container">
-    <div id="order_container" class="col-lg-8 col-xs-12 mx-auto">
-      <div class="row">
-        <div id="order_table">
-          <div class="empty">
-            <div class="text-center order">您的帳號目前沒有訂單</div>
-          </div>
-        </div> -->
+    <div class="container-fluid">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-10">
+          <div class="h3 text-center my-5">訂單資訊</div>
+            <template v-if="isShow">
+              <div class="text-center order mb-5">您的帳號目前沒有訂單</div>
+            </template>  
+            <template v-if="isShowOrder">
+              <table class="table table-hover mb-5">
+                <thead>
+                  <tr>
+                    <th scope="col">商品明細</th>
+                    <th scope="col">單價</th>
+                    <th scope="col">數量</th>
+                    <th scope="col">小計</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="orderItem in order" :key="orderItem.id">
+                    <td>
+                      <img class="product-pic" :src="orderItem.imageUrlOne" alt="" />
+                      <span class="product-name">{{ orderItem.title }}</span>
+                    </td>
 
-  <div class="container-fluid">
-    <div class="row d-flex justify-content-center">
-      <div class="col-lg-10">
-        <div class="h3 text-center my-5">訂單資訊</div>
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">商品明細</th>
-              <th scope="col">單價</th>
-              <th scope="col">數量</th>
-              <th scope="col">小計</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="orderItem in order" :key="orderItem.id">
-              <td>
-                <img class="product-pic" :src="orderItem.imageUrlOne" alt="" />
-                <span class="product-name">{{ orderItem.title }}</span>
-              </td>
+                    <td>
+                      <p class="price">NT${{ orderItem.price }}</p>
+                    </td>
 
-              <td>
-                <p class="price">NT${{ orderItem.price }}</p>
-              </td>
-
-              <td>
-                <div>{{orderItem.quantity}}</div>
-              </td>
-              <td>
-                <p class="price">
-                  NT${{ orderItem.quantity * orderItem.price }}
-                </p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                    <td>
+                      <div>{{orderItem.quantity}}</div>
+                    </td>
+                    <td>
+                      <p class="price">
+                        NT${{ orderItem.quantity * orderItem.price }}
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </template>
+        </div>
       </div>
     </div>
-  </div>
-
+  
 </template>
 
 <style scoped>
@@ -87,12 +83,19 @@ export default {
   data() {
     return {
       order: [],
+      isShow: false,
+      isShowOrder: true
     };
   },
   methods: {},
   mounted() {
     this.order = JSON.parse(localStorage.getItem("order")) || [];
     console.log(this.order);
+
+    if(this.order.length==0){
+      this.isShow = true
+      this.isShowOrder = false
+    }
   },
 };
 </script>
